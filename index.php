1906997302f0d5ae5d391be73492baeeed49017e
Arjunstrwb <arjuna.satrio1007@students.unila.ac.id>
2020-03-11 16:12:16 +0700

    update time refresh

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $resultFilter = isset($getFilter['data']) ? $getFilter['data'] : [];
 <!DOCTYPE html>
 <html>
 <head>
-	<!-- <meta http-equiv="refresh" content="60"> -->
+	<meta http-equiv="refresh" content="60">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>SIMUDA - Dashboard</title>
