b6e878d2587256f91c491462ee66ebb4e7ec0c09
bellabeen <rootsquid@gmail.com>
2020-03-23 22:36:18 +0700

    Delete file

--- conf/config.php
+++ conf/config.php
@@ -1,12 +1,7 @@
 <?php
 define("DB_HOST","localhost");
-<<<<<<< HEAD
 define("DB_USER","bellabeen");
 define("DB_PASSWORD","kepoajalu");
-=======
-define("DB_USER","root");
-define("DB_PASSWORD","");
->>>>>>> 82f9b5983ed184eb7e359d7a1b501090c1053503
 define("DB_NAME","3074218_latihanarjun");
 ?>
 
