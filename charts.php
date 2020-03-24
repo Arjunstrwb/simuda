c22951c0a196d24730bb08e37f7f102945903c8a
bellabeen <rootsquid@gmail.com>
2020-03-24 21:57:55 +0700

    Delete little

--- charts.php
+++ charts.php
@@ -24,10 +24,7 @@ $resultAll= isset($getAll['data']) ? $getAll['data'] : [];
 	
 	<Custom Fon>
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-	<[if lt IE 9]>
-	<script src="js/html5shiv.js"></script>
-	<script src="js/respond.min.js"></script>
-	<[endif]
+
 </head>
 <body>
 	<?php include "master/header.php"?>
